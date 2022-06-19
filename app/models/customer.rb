e32bc1cb1f40047a69f58eab8b9e44d53514c93e
r-ikemori <104720499+r-ikemori@users.noreply.github.com>
2022-06-19 04:47:16 +0000

    [add]admin_custmers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def fullname
+    last_name+first_name
+  end
+  def fullname_kana
+    last_name_kana+first_name_kana
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-    namespace :admin do
+  devise_for :customers,skip: [:registrations, :passwords], controllers: {
+    sessions: 'public/sessions'
+  }
+  devise_scope :customers do
+    get '/customers/sign_up', to: 'public/registrations#new'
+    post '/customers', to: 'public/registrations#create'
+  end
+  scope module: :public do
+    root to: 'homes#top'
+    get "/about" => "homes#about", as: "about"
+    resource :customers, only: [:show, :edit, :destroy, :update, :confirm]
+  end
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+
+  sessions: "admin/sessions"
+  }
+
+  namespace :admin do
     resources :genres, only: [:create, :edit, :index, :update]
     resources :items, only: [:create, :edit, :index, :update, :new, :show]
+    resources :customers, only: [:edit, :index, :update, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
